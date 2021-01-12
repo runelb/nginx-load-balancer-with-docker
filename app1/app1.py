5682694e5bf863f3da2b75bad298ff5e7cf4fc11
Rune Jidrot <rune@jidrot.se>
2021-01-12 19:28:21 +0100

    initial commit

---
+++ app1/app1.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import os
+import socket
+
+app = Flask(__name__)
+@app.route("/")
+
+def hello():
+    server_name = "Server 1"
+    html = "<h3>Hello, this is {name}!</h3> <b>Hostname:</b> {hostname}<br/>"
+    return html.format(name=server_name, hostname=socket.gethostname())
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
